63e4b6d874f50c5bd26c523ce568dbc130581dc1
Bruno André da Silva <38144738+brunoas17@users.noreply.github.com>
2024-12-11 15:57:12 +0000

    ajustando configuração

--- server.js
+++ server.js
@@ -26,12 +26,12 @@ const allowCors = (req, res, next) => {
 app.use(allowCors);
 app.use(bodyParser.json({ limit: "10mb" }));
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(__dirname + "/dist/lojaonline"));
+app.use(express.static(__dirname + "/dist"));
 
 // servir o index.html do angular
 
 app.get("*", (req, res) => {
-  res.sendFile(__dirname + "/dist/lojaonline/index.html");
+  res.sendFile(__dirname + "/dist/index.html");
 });
 
 app.listen(port, async () => {

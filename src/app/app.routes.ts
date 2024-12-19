996242915238cad08d1a3d78a3ede04c4af98160
Bruno Silva <brunoandredasilva17@gmail.com>
2024-12-19 17:04:34 -0300

    criando primeiros componentes e incluindo bootstrap

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
+import { RegisterComponent } from './modules/authentication/register/register.component';
 import { UnderConstructionComponent } from './modules/under-construction/under-construction.component';
 
 export const routes: Routes = [
   {
     path: '',
     component: UnderConstructionComponent
+  },
+  {
+    path: 'registrar-se',
+    component: RegisterComponent
   }
 ];
